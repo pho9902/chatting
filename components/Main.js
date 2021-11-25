ab78f6202539c6c4c854734cdf2a2553b615852f
unknown <pho9902@naver.com>
2021-11-25 17:31:41 +0900

    레이아웃

--- components/Main.js
+++ components/Main.js
@@ -1,25 +1,62 @@
 import styled from "styled-components";
 
+const WrapDiv = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`;
+
 const BodyDiv = styled.div`
   border: 1px solid;
   display: flex;
   flex-direction: column;
-  width: 100vw;
-  height: 100vh;
+  width: 60vw;
+  height: 80vh;
 `;
 
 const Header = styled.h4`
   color: red;
 `;
 
+const Users = styled.div`
+  width: 20vw;
+  border: 1px solid;
+`;
+const UserHeader = styled.div`
+  width: 100%;
+  border: 1px solid;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const Input = styled.div`
+  border: 1px solid;
+  width: 100%;
+  height: 10vh;
+`;
+
+const SendBtn = styled.button`
+  padding: 1rem;
+  border: 1px solid;
+`;
+
 const Main = () => {
   return (
-    <div>
+    <WrapDiv>
       <BodyDiv>
         <Header>채팅을 만들어보자</Header>
         외않되?
+        <Input>전송</Input>
       </BodyDiv>
-    </div>
+      <Users>
+        <UserHeader>
+          <span>채팅 참여자 목록</span>
+        </UserHeader>
+        <span>박현우</span>
+      </Users>
+    </WrapDiv>
   );
 };
 
